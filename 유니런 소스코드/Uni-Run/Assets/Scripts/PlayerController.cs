d8f18859cfae9cef80a52665fe8cd3ca9a4dc429
kyunjun98 <kyunjun98@naver.com>
2019-12-04 19:17:38 +0900

    떨어져 죽어도 계속 플레이가 되는 오류 수정

--- 유니런 소스코드/Uni-Run/Assets/Scripts/PlayerController.cs
+++ 유니런 소스코드/Uni-Run/Assets/Scripts/PlayerController.cs
@@ -49,4 +49,24 @@ public class PlayerController : MonoBehaviour {
         // 애니메이터의 Grounded 파라미터를 isGrounded 값으로 갱신
         animator.SetBool("Grounded", isGrounded);
     }
+
+    private void Die() {
+        // 애니메이터의 Die 트리거 파라미터를 셋
+        animator.SetTrigger("Die");
+
+        // 오디오 소스에 할당된 오디오 클립을 deathClip으로 변경
+        playerAudio.clip = deathClip;
+        // 사망 효과음 재생
+        playerAudio.Play();
+
+        // 속도를 제로(0, 0)로 변경
+        playerRigidbody.velocity = Vector2.zero;
+        // 사망 상태를 true로 변경
+        isDead = true;
+
+        // 게임 매니저의 게임 오버 처리 실행
+        GameManager.instance.OnPlayerDead();
+    }
+
+
 }
